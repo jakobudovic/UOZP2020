eb889d721577119e784cf2324cf4ef9b47e689a1
jakobudovic <ju8168@student.uni-lj.si>
2020-10-23 10:48:37 +0200

    Added histogram script that shows points of voting are distributed.

---
+++ 01/histogram.py
@@ -0,0 +1,33 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+
+file_name = "eurovision-finals-1975-2019.csv"
+f = open(file_name, "rt")
+values = csv.reader(f)
+header_line = next(values)  # skip header cell
+values = np.array(list(values))  # values used for unique countries extraction
+print("header_line:", header_line)
+
+points = values[:,4]
+
+points1 = [l for l in points if l != 0]
+# points1 = [x if x % 2 else x * 100 for x in range(1, 10) ]
+
+points2 = []
+
+for l in points:
+    # print(type(int(l)))
+    if int(l) > 0:
+      points2.append(l)
+
+print("points2:", points2)
+# print(values[:,4])
+# print(len(values[:,4]))
+
+plt.bar(points, np.arange(len(points)))
+# plt.hist(points2, bins=12)
+# plt.bar(points, np.arange(len(points)))
+plt.show()
+
+print(len(points2))
